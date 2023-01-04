4002b98b05f80db279b3719b906bb457a8e49b4f
Danil <timerbulatov_danilka@mail.ru>
2023-01-04 19:38:35 +0300

    update behavior in case of empty stack, add test

--- stack/stack.go
+++ stack/stack.go
@@ -20,6 +20,10 @@ func (s *Stack[T]) Push(value T) {
 
 // Pop removes the most recently added element.
 func (s *Stack[T]) Pop() T {
+	if len(s.array) == 0 {
+		var zero T
+		return zero
+	}
 	v := s.array[len(s.array)-1]
 	s.array = s.array[:len(s.array)-1]
 	return v
@@ -27,10 +31,21 @@ func (s *Stack[T]) Pop() T {
 
 // Peek returns element on top of stack.
 func (s *Stack[T]) Peek() T {
+	if len(s.array) == 0 {
+		var zero T
+		return zero
+	}
 	return s.array[len(s.array)-1]
 }
 
 // Size returns size of the stack.
 func (s *Stack[T]) Size() int {
 	return len(s.array)
+}
+
+// All returns all elements from the stack.
+func (s *Stack[T]) All() []T {
+	all := make([]T, len(s.array))
+	copy(all, s.array)
+	return all
 }
--- stack/stack_test.go
+++ stack/stack_test.go
@@ -1,10 +1,15 @@
 package stack
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/Tv0ridobro/data-structure/slices"
+)
 
 func TestStack_Peek(t *testing.T) {
 	t.Parallel()
 	stack := New[int]()
+	stack.Peek()
 	stack.Push(0)
 	stack.Peek()
 	if v := stack.Peek(); v != 0 {
@@ -38,9 +43,12 @@ func TestStack_Peek(t *testing.T) {
 func TestStack_Pop(t *testing.T) {
 	t.Parallel()
 	stack := New[int]()
-	stack.Push(0)
 	if v := stack.Pop(); v != 0 {
 		t.Errorf("wrong pop %d != %d", v, 0)
+	}
+	stack.Push(90)
+	if v := stack.Pop(); v != 90 {
+		t.Errorf("wrong pop %d != %d", v, 90)
 	}
 	stack.Push(99)
 	if v := stack.Pop(); v != 99 {
@@ -51,14 +59,70 @@ func TestStack_Pop(t *testing.T) {
 func TestStack_Size(t *testing.T) {
 	t.Parallel()
 	stack := New[int]()
+	if stack.Size() != 0 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(0)
+	if stack.Size() != 1 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(1)
+	if stack.Size() != 2 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(0)
+	if stack.Size() != 3 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(1)
+	if stack.Size() != 4 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(0)
+	if stack.Size() != 5 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Pop()
+	if stack.Size() != 4 {
+		t.Errorf("wrong size of stack")
+	}
 	stack.Push(1)
 	if stack.Size() != 5 {
 		t.Errorf("wrong size of stack")
+	}
+	stack.Pop()
+	if stack.Size() != 4 {
+		t.Errorf("wrong size of stack")
+	}
+	stack.Pop()
+	if stack.Size() != 3 {
+		t.Errorf("wrong size of stack")
+	}
+}
+
+func TestStack_All(t *testing.T) {
+	t.Parallel()
+	stack := New[int]()
+	if !slices.Equal(stack.All(), []int{}) {
+		t.Errorf("wrong all %v", []int{})
+	}
+	stack.Push(0)
+	stack.Push(1)
+	stack.Push(2)
+	stack.Push(3)
+	stack.Push(4)
+	if !slices.Equal(stack.All(), []int{0, 1, 2, 3, 4}) {
+		t.Errorf("wrong all %v", []int{0, 1, 2, 3, 4})
+	}
+	stack.Pop()
+	if !slices.Equal(stack.All(), []int{0, 1, 2, 3}) {
+		t.Errorf("wrong all %v", []int{0, 1, 2, 3})
+	}
+	stack.Push(7)
+	if !slices.Equal(stack.All(), []int{0, 1, 2, 3, 7}) {
+		t.Errorf("wrong all %v", []int{0, 1, 2, 3, 7})
+	}
+	if !slices.Equal(stack.All(), []int{0, 1, 2, 3, 7}) {
+		t.Errorf("wrong all %v", []int{0, 1, 2, 3, 7})
 	}
 }
